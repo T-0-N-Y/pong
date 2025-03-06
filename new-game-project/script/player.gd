1bfcc0c343e781d815ea22df16c9f02a7f19f2a7
T-0-N-Y <198862472+T-0-N-Y@users.noreply.github.com>
2025-03-06 16:32:39 +1100

    paddle moving cries

--- new-game-project/script/player.gd
+++ new-game-project/script/player.gd
@@ -5,17 +5,17 @@ var winHeight : int
 var pHeight : int
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	#get window height
 	winHeight = get_viewport_rect().size.y
 	#get paddle height by getting the colorRect node
 	pHeight = $ColorRect.get_size().y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui.up"):
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
 		#get_parent allows me to see var in the parent node
 		position.y -= get_parent().PADDLE_SPEED * delta
-	elif Input.is_action_pressed("ui.down"):
+	elif Input.is_action_pressed("ui_down"):
 		#get_parent allows me to see var in the parent node
 		position.y += get_parent().PADDLE_SPEED * delta
