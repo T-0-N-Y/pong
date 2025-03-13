9db654ef0664e4c2653cec8d37c211c8a7a02cfc
T-0-N-Y <198862472+T-0-N-Y@users.noreply.github.com>
2025-03-13 15:49:26 +1100

    it works finally i think

---
+++ new-game-project/script/ball.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var winSize : Vector2
+const START_SPEED : int = 500
+const accel : int = 50
+var speed : int
+var dir : Vector2
+
+func _ready():
+	winSize = get_viewport_rect().size
+	#print("hi")
+
+func new_ball():
+	print("hi")
+	position.x = winSize.x / 2
+	position.y = randi_range(200, winSize.y - 200)
+	speed = START_SPEED
+	dir = random_direction()
+
+func _physics_process(delta):
+	move_and_collide (dir * speed * delta)
+
+
+func random_direction():
+	var newDir := Vector2()
+	newDir.x = [1, -1].pick_random()
+	newDir.y = randf_range(-1,1)
+	return newDir.normalized()
--- new-game-project/script/main.gd
+++ new-game-project/script/main.gd
@@ -3,6 +3,9 @@ extends Sprite2D
 var score := [0, 0] #array to keep track of score
 const PADDLE_SPEED : int = 100 #paddle speed
 
+func _on_timer_timeout():
+	print("pls trigger")
+	$ball.new_ball()
 
 # # Called when the node enters the scene tree for the first time.
 # func _ready() -> void:
@@ -12,4 +15,3 @@ const PADDLE_SPEED : int = 100 #paddle speed
 # # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta: float) -> void:
 # 	pass
-
