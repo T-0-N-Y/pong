f386130d5844119c67d5b8faafaebe3d77518a99
T-0-N-Y <198862472+T-0-N-Y@users.noreply.github.com>
2025-03-17 22:25:28 +1100

    added lose noise

--- new-game-project/script/main.gd
+++ new-game-project/script/main.gd
@@ -2,11 +2,13 @@ extends Sprite2D
 
 var score := [0, 0] #array to keep track of score
 const PADDLE_SPEED : int = 100 #paddle speed
+var game_over : bool = false
 
 func _on_timer_timeout():
-	if score[1] < 1 and score[0] < 1:
+	if !game_over:
 		$ballReal.new_ball()
 		$HUD/result.text = ""
+		$HUD/win.text = ""
 
 func _on_score_left_body_entered(body: Node2D) -> void:
 	if score[1] < 1:
@@ -16,7 +18,11 @@ func _on_score_left_body_entered(body: Node2D) -> void:
 		$Timer.start()
 	if score[1] == 1:
 		$HUD/result.text = ("YOU LOSE")
-	
+		game_over = true
+		$Timer.stop()
+		$loseSound.stream.loop = true
+		$loseSound.play()
+		
 func _on_score_right_body_entered(body: Node2D) -> void:
 	if score[0] < 1:
 		score[0] += 1
@@ -25,3 +31,17 @@ func _on_score_right_body_entered(body: Node2D) -> void:
 		$Timer.start()
 	if score[0] == 1:
 		$HUD/win.text = ("YOU WIN")
+		game_over = true
+		$Timer.stop() 
+		
+func _input(event):
+	if game_over and event.is_action_pressed("ui_accept"):  # Space is detected after losing
+		score[0] = 0
+		score[1] = 0
+		game_over = false  # Reset the flag
+		$HUD/result.text = ""
+		$HUD/win.text = ""
+		$HUD/CPUScore.text = str(score[1])
+		$HUD/playerScore.text = str(score[0])
+		$Timer.start()
+		$loseSound.stop()
